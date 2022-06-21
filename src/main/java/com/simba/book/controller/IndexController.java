6e1ca4b31cf7e780949e3a5d8e4a5c31f3239030
SiJun-Yoo <simba0502@naver.com>
2022-06-21 22:39:54 +0900

    중간 push

--- src/main/java/com/simba/book/controller/IndexController.java
+++ src/main/java/com/simba/book/controller/IndexController.java
@@ -1,10 +1,13 @@
 package com.simba.book.controller;
 
+import com.simba.book.dto.PostResponseDto;
 import com.simba.book.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -16,4 +19,13 @@ public class IndexController {
         model.addAttribute("posts",postService.findAllDesc());
         return "index";
     }
+
+    @GetMapping("/post/update/{id}")
+    public String postUpdate(@PathVariable Long id, Model model){
+        PostResponseDto dto = postService.findById(id);
+        model.addAttribute("post",dto);
+        return "post-update";
+    }
+
+
 }
